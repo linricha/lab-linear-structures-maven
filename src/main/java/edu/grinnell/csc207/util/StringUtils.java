deb27d05f1929d8f6cdace74d7620bfd1622879b
linricha <linricha@grinnell.edu>
2024-10-03 14:47:07 -0500

    Implement the  method.

--- src/main/java/edu/grinnell/csc207/util/StringUtils.java
+++ src/main/java/edu/grinnell/csc207/util/StringUtils.java
@@ -9,7 +9,8 @@ import java.io.PrintWriter;
  * Assorted utilities for working with strings.
  * 
  * @author Samuel A. Rebelsky
- * @author Your Name Here
+ * @author Richard Lin 
+ * @author Anthony Castleberry
  */ 
 public class StringUtils {
   // +------------------+--------------------------------------------
@@ -19,9 +20,32 @@ public class StringUtils {
   /**
    * Determine whether the parens match in string.
    */
-  public static boolean checkMatching(String str) {
+  public static boolean checkMatching(String str) throws Exception{
     Stack<Character> parens = new LinkedStack<Character>();
-    return false;       // STUB
+    for(int i = 0; i < str.length(); i++) { 
+      if (str.charAt(i) == '(') {
+        parens.put('(');
+      } else if (str.charAt(i) == '[') {
+        parens.put('[');
+      } else if (str.charAt(i) == ')') {
+        if (parens.isEmpty()) {
+          return false;
+        } // if
+        Character placeholder = parens.get();
+        if (placeholder != '(') {
+          return false;
+        } // if
+      } else if (str.charAt(i) == ']') {
+        if (parens.isEmpty()) {
+          return false;
+        } // if
+        Character placeholder = parens.get();
+        if (placeholder != '[') {
+          return false;
+        } // if
+      } // if
+    } // for
+    return parens.isEmpty();
   } // checkMatching
 } // class StringUtils    
 
